26dbb1a5f6f862023ee4f77c264e31f5a1e9baa7
Jorge Renteral <jorgerenteral@gmail.com>
2022-06-14 21:44:10 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dharma-freelance-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dharma-freelance';
+}
